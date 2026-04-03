88f7f76fa4c98f4fccc23bcaafc94dd04dbd1220
Nikhilwakekar01 <nikhilvakekars@gmail.com>
2026-04-03 13:54:46 +0530

    cart fix

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import CartItems from "../components/cart/CartItems";
-import PriceDetails from "../components/cart/PriceDetails";
+import CartItems from "../components/Cart/CartItems";
+import PriceDetails from "../components/Cart/PriceDetails";
 
 const Cart = () => {
   return (
-    <main className="min-h-screen bg-gray-50 pt-0"> 
+    <main className="min-h-screen bg-gray-50 pt-0">
       {/* Added top padding because your navbar is fixed */}
-      
+
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        
+
 
         <div className="grid gap-6 lg:grid-cols-3">
-          
+
           {/* Cart Items Column */}
           <div className="lg:col-span-2">
             <CartItems />
